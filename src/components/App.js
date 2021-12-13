1f31cc41ed510afe7bd338096db732e953b39cc4
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-12-13 16:25:56 +0530

    initial setup

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React,{Component} from "react";
+class App extends Component{
+    render(){
+        return(
+            <div>
+               <h4>Meme Generator !</h4> 
+            </div>
+        )
+    }
+}
+export default App;
